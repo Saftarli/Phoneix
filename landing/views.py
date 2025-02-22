645b62ff90bbe79cdf9f0505e858fd1a38cf41e4
saftarli <tofiqsefterli@gmail.com>
2025-02-22 20:06:24 +0400

    about.html

--- landing/views.py
+++ landing/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
      return render(request,'landing/index.html', {})
+
+def about(request):
+     return render(request,'landing/about.html', {})
